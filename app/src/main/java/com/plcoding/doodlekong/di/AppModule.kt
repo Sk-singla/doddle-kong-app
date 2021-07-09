bbdaee3e60b91e6b02d9fd094f729af0b07ebfd0
SuperSk <samarashu02@gmail.com>
2021-07-09 23:50:44 +0530

    retrofit setup

---
+++ app/src/main/java/com/plcoding/doodlekong/di/AppModule.kt
@@ -0,0 +1,69 @@
+package com.plcoding.doodlekong.di
+
+import com.google.gson.Gson
+import com.plcoding.doodlekong.data.remote.api.SetupApi
+import com.plcoding.doodlekong.util.Constants.HTTP_BASE_URL
+import com.plcoding.doodlekong.util.Constants.HTTP_BASE_URL_LOCALHOST
+import com.plcoding.doodlekong.util.Constants.USE_LOCALHOST
+import com.plcoding.doodlekong.util.DispatcherProvider
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .build()
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideGsonInstance(): Gson {
+        return Gson()
+    }
+
+    @Singleton
+    @Provides
+    fun provideDispatcherProvider(): DispatcherProvider {
+        return object : DispatcherProvider {
+            override val main: CoroutineDispatcher
+                get() = Dispatchers.Main
+            override val io: CoroutineDispatcher
+                get() = Dispatchers.IO
+            override val default: CoroutineDispatcher
+                get() = Dispatchers.Default
+        }
+    }
+
+    @Singleton
+    @Provides
+    fun provideSetupApi(
+        okHttpClient: OkHttpClient
+    ): SetupApi {
+        return Retrofit.Builder()
+            .baseUrl(if(USE_LOCALHOST) HTTP_BASE_URL_LOCALHOST else HTTP_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+            .create(SetupApi::class.java)
+    }
+
+
+}
---
+++ app/src/main/java/com/plcoding/doodlekong/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.plcoding.doodlekong.util
+
+object Constants {
+
+    const val USE_LOCALHOST = true
+
+    const val HTTP_BASE_URL = ""
+    const val HTTP_BASE_URL_LOCALHOST = "http://192.168.56.1:8081"
+
+}
