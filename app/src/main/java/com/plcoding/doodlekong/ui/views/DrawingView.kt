f36ce007bc75fd0290f4309812e64c5c713929a2
SuperSk <samarashu02@gmail.com>
2021-07-11 11:37:40 +0530

    Drawing view done

--- app/src/main/java/com/plcoding/doodlekong/ui/views/DrawingView.kt
+++ app/src/main/java/com/plcoding/doodlekong/ui/views/DrawingView.kt
@@ -3,9 +3,12 @@ package com.plcoding.doodlekong.ui.views
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.MotionEvent.*
 import android.view.View
 import com.plcoding.doodlekong.util.Constants
 import java.util.*
+import kotlin.math.abs
 
 class DrawingView @JvmOverloads constructor(
     context: Context,
@@ -34,6 +37,7 @@ class DrawingView @JvmOverloads constructor(
 
     private var path = Path()
     private var paths = Stack<PathData>()
+    private var undoPaths = Stack<PathData>() // BY MY OWN
     private var pathDataChangedListener: ((Stack<PathData>) -> Unit)? = null
 
     fun setPathDataChangedListener(listener: (Stack<PathData>)-> Unit){
@@ -51,8 +55,107 @@ class DrawingView @JvmOverloads constructor(
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        
+
+        val initialColor = paint.color
+        val initialThickness = paint.strokeWidth
+        for(pathData in paths){
+            paint.apply {
+                color = pathData.color
+                strokeWidth = pathData.thickness
+            }
+            canvas?.drawPath(pathData.path,paint)
+        }
+
+        paint.apply {
+            color = initialColor
+            strokeWidth = initialThickness
+        }
+        canvas?.drawPath(path,paint)
     }
+
+
+
+    private fun startedTouch(x:Float, y: Float) {
+        path.reset()
+        path.moveTo(x,y)
+        curX = x
+        curY = y
+        undoPaths.clear()
+        invalidate()
+    }
+
+    // Todo: Quadratic Bezier (Google it)
+    // Todo: Lottie Animation
+    // Todo: Canvas
+
+    private fun moveTouch(toX: Float, toY: Float) {
+        val dx = abs(toX - (curX ?: return))
+        val dy = abs(toY - (curY ?: return))
+        if(dx >= smoothness || dy >= smoothness){
+            isDrawing = true
+            path.quadTo(curX!!, curY!!, (curX!! + toX) / 2f, (curY!! + toY) / 2f)
+            curX = toX
+            curY = toY
+            invalidate()
+        }
+    }
+    private fun releaseTouch(){
+        isDrawing = false
+        path.lineTo(curX ?: return, curY ?: return)
+        paths.push(PathData(path,paint.color,paint.strokeWidth))
+        pathDataChangedListener?.let { change ->
+            change(paths)
+        }
+        path = Path()
+        invalidate()
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if(!isEnabled){
+            return false
+        }
+        val newX = event?.x
+        val newY = event?.y
+        when(event?.action){
+            ACTION_DOWN -> startedTouch(newX ?: return false, newY ?: return false)
+            ACTION_MOVE -> moveTouch(newX ?: return false, newY ?: return false)
+            ACTION_UP -> releaseTouch()
+
+        }
+        return true
+    }
+
+    fun setThickness(thickness: Float){
+        paint.strokeWidth = thickness
+    }
+
+    fun setColor(color:Int){
+        paint.color = color
+    }
+
+    fun clear(){
+        canvas?.drawColor(Color.TRANSPARENT,PorterDuff.Mode.MULTIPLY)
+        paths.clear()
+    }
+
+    fun undo(){
+        if(!paths.empty()) {
+            paths.pop()?.let {
+                undoPaths.push(it)
+                invalidate()
+            }
+        }
+    }
+    fun redo(){
+        if(!undoPaths.empty()) {
+            undoPaths.pop()?.let {
+                paths.push(it)
+                invalidate()
+            }
+        }
+    }
+
+
 
     data class PathData(val path: Path,val color: Int, val thickness: Float)
 
